ea4742318974123c9b841cad46136fc6f4838234
PraveenM24 <praveen050800@gmail.com>
2021-03-09 14:31:28 +0530

    Initial commit

--- models/alien2.js
+++ models/alien2.js
@@ -10,6 +10,22 @@ const dbSchema = new mongoose.Schema({
     lastname: {
         type: String,
         required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    batch: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
     }
 
 })
